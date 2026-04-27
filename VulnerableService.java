1cedb62ba46070bce981a4ae7a9cb0579ed5d5e8
Asawari Kulkarni <asawariku@cybage.com>
2026-04-27 12:15:23 +0530

    Add vulnerable.java

---
+++ VulnerableService.java
@@ -0,0 +1,9 @@
+public class VulnerableService {
+    public void connect() {
+        String username = "admin";
+        String password = "admin123";
+        String apiKey = "sk_live_123456";
+
+        System.out.println("Connecting with " + username);
+    }
+}
